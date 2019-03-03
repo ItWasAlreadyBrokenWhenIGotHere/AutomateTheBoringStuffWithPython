3b1bc69f281b086d45ed8c0b5c4195d404d4b605
Wecke <petri.weckstrom@gmail.com>
2019-03-03 21:38:17 +0200

    S12 completed

---
+++ debugging/logging_example.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Module Docstring
+"""
+
+__author__ = "Petri Weckström"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+import logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+# logging.disable(logging.CRITICAL) # This disable all logs as CRITICAL level is highest
+"""
+Log Levels:
+debug (lowest)
+info
+warning
+error
+critical (highest)
+"""
+
+def main():
+    """ Main entry point of the app """
+    print("hello world")
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()
---
+++ debugging/traceback_example.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Module Docstring
+"""
+
+__author__ = "Petri Weckström"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+import traceback
+
+
+def main():
+    """ Main entry point of the app """
+    try:
+        raise Exception('This is the error message.')
+    except:
+        errorFile = open('error_log.txt', 'a')
+        errorFile.write(traceback.format_exc())
+        errorFile.close()
+        print('The traceback info was written to error_log.txt')
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()
