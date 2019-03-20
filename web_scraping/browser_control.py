58196a9ea4c72738e0d5784f570cdbee1a700d44
Petri Weckström <petri.weckstrom@gmail.com>
2019-03-20 20:17:31 +0200

    S13 completed

---
+++ web_scraping/browser_control.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Module Docstring
+"""
+
+__author__ = "Petri Weckström"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+from selenium import webdriver
+
+import time
+import logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+# logging.disable(logging.CRITICAL) # This disable all logs as CRITICAL level is highest
+
+
+def main():
+    """ Main entry point of the app """
+    logging.debug('Start of Selenium Browser control')
+    browser = webdriver.Chrome(executable_path='E:/Git/automation/web_scraping/chromedriver.exe')
+    logging.debug('Open www.google.com')
+    browser.get('https://www.google.com')
+    # element for the search field
+    searchElem = browser.find_element_by_css_selector('#tsf > div:nth-child(2) > div > div.RNNXgb > div > div.a4bIc > input')
+    searchElem.send_keys('cat video')
+    searchElem.submit()
+    time.sleep(5)  # Delays for 5 seconds. You can also use a float value.
+    browser.back()
+    time.sleep(2)
+    browser.quit()
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()
---
+++ web_scraping/download_file_using_requests.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Module Docstring
+"""
+
+__author__ = "Petri Weckström"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+import requests
+
+import logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+# logging.disable(logging.CRITICAL) # This disable all logs as CRITICAL level is highest
+
+
+def main():
+    """ Main entry point of the app """
+    res = requests.get('https://automatetheboringstuff.com/files/rj.txt')
+    print(res.status_code)
+    print(res.text[:500])  # prints 500 characters from the file (Romeo and Juliet)
+
+    # Example about bad request and how to show error using raise_for_status
+    #badRes = requests.get('https://automatetheboringstuff.com/kjdfnksdjndfkajn')
+    #badRes.raise_for_status()
+
+    # To write response to file
+    playFile = open('RomeoAndJuliet.txt', 'wb')  # content needs to be written in binary mode (wb)
+    for chunk in res.iter_content(100000): # writing is done in iterations of 100000 char per loop
+        playFile.write(chunk)
+
+    playFile.close()
+
+
+if __name__ == "__main__":
+    main()
